7c1c5ec3a8e1c804fe269da8542d498f9b6ba3f7
root <root@global03.ecnivconcepts.nl>
2023-03-27 13:29:50 +0200

    Scan form

--- aanmelden.php
+++ aanmelden.php
@@ -1,18 +1,63 @@
-</php
+<?php
+include("config.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	
+	$knsanummer = $_REQUEST['knsa'];
+	$bezoekers = array();
+	if ($file = fopen("baanplanner_export.csv", "r"))
+	{
+		while(!feof($file))
+		{
+			$line = fgets($file);
+			$arr = explode(";",$line);
+
+			$regel['knsa'] = trim($arr[16]);
+			$knsa = $regel['knsa'];
+			$bezoekers[$knsa]['aantal']++;
+			if(trim($arr[20]) == "KKP")
+			{
+				$bezoeker[$knsa]['KKP'] = 1;
+			}
+			else if(trim($arr[20]) == "GKP")
+			{
+				$bezoeker[$knsa]['GKP'] = 1;
+			}
+				
+		 
+		   $i++;
+		}
+		fclose($file);
+	}
+
+	if($bezoekers[$knsanummer]['aantal'] < 1)
+	{
+		print "deze bezoeker komt niet voor";
+	}
+	else
+	{
+		$totaalprijs = $bezoekers[$knsanummer]['aantal']*$wedstrijdprijs;
+		print "Kosten: $totaalprijs<br>
+		Aanmelden binnen Matchmaker<br>
+		
+		
+		";
+	}
+}
 
 
-
+		print "
+		<html>
+		<head>
+		</head>
+		<body>
+		<form action=\"aanmelden.php\" method=\"POST\">
+		  KNSA-licentienummer: <input type=\"text\" name=\"knsa\" value=\"\" autofocus><br>
+		  <input type=\"submit\" value=\"Submit\">
+		</form>
+		";
 
 
 
 ?>
-
-
-<html>
-<head>
-</head>
-<body>
-<form action="aanmelden.php">
-  KNSA-licentienummer: <input type="text" name="knsa" value=""><br>
-  <input type="submit" value="Submit">
-</form>
