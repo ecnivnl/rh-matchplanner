6ec80ef2707ac56ca36a808f75ef297880be4085
root <root@global03.ecnivconcepts.nl>
2023-03-27 12:23:45 +0200

    renamed formulier

---
+++ formulier.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+<form action="pdf.php">
+  Voorletter: <input type="text" name="voorletter" value=""><br>
+  Achternaam: <input type="text" name="achternaam" value=""><br>
+  KNSA-licentienummer: <input type="text" name="knsa" value=""><br>
+  Dicipline: <input type="text" name="dicipline" value=""><br>
+  Baannummer: <input type="text" name="baan" value=""><br>
+  Tijdstip: <input type="text" name="tijd" value=""><br>
+  <input type="submit" value="Submit">
+</form>
