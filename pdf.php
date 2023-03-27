bcb37397445d938767ef73693706aebfcb9d90c4
root <root@global03.ecnivconcepts.nl>
2023-03-27 10:26:54 +0200

    Made pdf universal by adding config lines

--- pdf.php
+++ pdf.php
@@ -1,105 +1,111 @@
 <?php
 
-	require_once("fpdf/fpdf.php");
-	
-	// $achternaam = html_entity_decode($_GET['achternaam']);
-	$achternaam = iconv('UTF-8', 'windows-1252', html_entity_decode($_GET['achternaam']));
+$vereniging['naam'] = "SSV Robin Hood";
+$vereniging['adres'] = "Parklaan 82";
+$vereniging['postcode'] = "3448ZM";
+$vereniging['plaats'] = "Woerden";
+$vereniging['knsa'] = "2570";
 
-	$pdf=new FPDF('P','mm','A4');
-	$pdf->AliasNbPages();
-	$pdf->AddPage();
-	$page++;
-	$pdf->SetFont('Helvetica','B',14);
-	$pdf->Cell(50,10,'Scoreformulier Wedstrijd Service Pistool 26-03-2023 - '.$_GET['tijd']." (baan ".$_GET['baan'].")",0,0);
-	$pdf->Ln(6);
-	$pdf->SetFont('Helvetica','B',12);
-	$naam = ltrim($_GET['voorletter']." ".$achternaam);
-	$pdf->Cell(10,10,$naam,0,0);$pdf->SetFont('Helvetica','',12);
-	$pdf->Ln(6);
-	$pdf->Cell(10,10,"KNSA-nummer: ".$_GET['knsa'],0,0);
-	$pdf->Ln(6);
-	$pdf->Cell(10,10,"Dicipline: ".$_GET['dicipline'],0,0);
-	$pdf->Ln(6);$pdf->Ln(6);
-	$pdf->SetFont('Helvetica','B',12);
+require_once("fpdf/fpdf.php");
 
-	$pdf->Cell(10,10,"Uitslag / wedstrijdbriefje",'',12);
-	$pdf->Ln(6);
-	$pdf->SetFont('Helvetica','B',12);
+// $achternaam = html_entity_decode($_GET['achternaam']);
+$achternaam = iconv('UTF-8', 'windows-1252', html_entity_decode($_GET['achternaam']));
 
-	
-	
-	$pdf->Cell(50,10,'Schoten links',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Ln(10);
-	$pdf->Cell(50,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Ln(10);
-	$pdf->Ln(10);
+$pdf=new FPDF('P','mm','A4');
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$page++;
+$pdf->SetFont('Helvetica','B',14);
+$pdf->Cell(50,10,'Scoreformulier Wedstrijd Service Pistool 26-03-2023 - '.$_GET['tijd']." (baan ".$_GET['baan'].")",0,0);
+$pdf->Ln(6);
+$pdf->SetFont('Helvetica','B',12);
+$naam = ltrim($_GET['voorletter']." ".$achternaam);
+$pdf->Cell(10,10,$naam,0,0);$pdf->SetFont('Helvetica','',12);
+$pdf->Ln(6);
+$pdf->Cell(10,10,"KNSA-nummer: ".$_GET['knsa'],0,0);
+$pdf->Ln(6);
+$pdf->Cell(10,10,"Dicipline: ".$_GET['dicipline'],0,0);
+$pdf->Ln(6);$pdf->Ln(6);
+$pdf->SetFont('Helvetica','B',12);
 
-	$pdf->Cell(50,10,'Schoten rechts',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Ln(10);
-	$pdf->Cell(50,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	$pdf->Cell(20,10,'',1,0);
-	
-	$pdf->Ln(10);$pdf->Ln(10);
-	// $pdf->SetFont('Helvetica','B',12);
-	// $pdf->Cell(50,10,'Totaalscore',1,0);
-	// $pdf->SetFont('Helvetica','',12);
-	// $pdf->Cell(50,10,'',1,0);
-	// $pdf->Cell(50,10,'  (Totale score, dus bijv. 238)',0,0);
-	// $pdf->Ln(10);
-	// $pdf->SetFont('Helvetica','',9);
-	// $pdf->Cell(50,10,'Indien sprake van diskwalificatie, een kruis plaatsen door de scorevlakken en bij totaalscore DSQ schrijven',0,0);
-	// $pdf->Ln(10);
-	// $pdf->Ln(10);
-	$pdf->SetFont('Helvetica','B',12);
-	$pdf->Cell(50,10,'Opmerking(en) van Baancommandant',0,0);
-	$pdf->Ln(10);$pdf->Ln(10);$pdf->Ln(10);
-	$pdf->SetFont('Helvetica','B',12);
-	$pdf->Cell(50,10,'Handtekening voor akkoord',0,0);
-	$pdf->Ln(10);
+$pdf->Cell(10,10,"Uitslag / wedstrijdbriefje",'',12);
+$pdf->Ln(6);
+$pdf->SetFont('Helvetica','B',12);
 
-	$pdf->SetFont('Helvetica','B',12);
-	$pdf->Cell(80,25,'Schutter',1,0);
-	
-	$pdf->Cell(80,25,'',1,0);
-	$pdf->Ln(25);
-	$pdf->Cell(50,10,'Verwerkt in MatchMaker',0,0);
 
-	$pdf->Ln(10);
-	$pdf->SetFont('Helvetica','B',12);
-	$pdf->Cell(80,25,'Administratie',1,0);
-	$pdf->SetFont('Helvetica','',12);
-	$pdf->Cell(80,25,'',1,0);
-	$pdf->Ln(40);
+
+$pdf->Cell(50,10,'Schoten links',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Ln(10);
+$pdf->Cell(50,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Ln(10);
+$pdf->Ln(10);
+
+$pdf->Cell(50,10,'Schoten rechts',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Ln(10);
+$pdf->Cell(50,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+$pdf->Cell(20,10,'',1,0);
+
+$pdf->Ln(10);$pdf->Ln(10);
+// $pdf->SetFont('Helvetica','B',12);
+// $pdf->Cell(50,10,'Totaalscore',1,0);
+// $pdf->SetFont('Helvetica','',12);
+// $pdf->Cell(50,10,'',1,0);
+// $pdf->Cell(50,10,'  (Totale score, dus bijv. 238)',0,0);
+// $pdf->Ln(10);
+// $pdf->SetFont('Helvetica','',9);
+// $pdf->Cell(50,10,'Indien sprake van diskwalificatie, een kruis plaatsen door de scorevlakken en bij totaalscore DSQ schrijven',0,0);
+// $pdf->Ln(10);
+// $pdf->Ln(10);
+$pdf->SetFont('Helvetica','B',12);
+$pdf->Cell(50,10,'Opmerking(en) van Baancommandant',0,0);
+$pdf->Ln(10);$pdf->Ln(10);$pdf->Ln(10);
+$pdf->SetFont('Helvetica','B',12);
+$pdf->Cell(50,10,'Handtekening voor akkoord',0,0);
+$pdf->Ln(10);
+
+$pdf->SetFont('Helvetica','B',12);
+$pdf->Cell(80,25,'Schutter',1,0);
+
+$pdf->Cell(80,25,'',1,0);
+$pdf->Ln(25);
+$pdf->Cell(50,10,'Verwerkt in MatchMaker',0,0);
+
+$pdf->Ln(10);
+$pdf->SetFont('Helvetica','B',12);
+$pdf->Cell(80,25,'Administratie',1,0);
+$pdf->SetFont('Helvetica','',12);
+$pdf->Cell(80,25,'',1,0);
+$pdf->Ln(40);
+
+
+$pdf->Image('logo_rh.png',10,230,30);
+$pdf->Cell(40,12,'',0,0);$pdf->Cell(80,12,$vereniging['naam'],0,0);$pdf->Ln(6);
+$pdf->Cell(40,12,'',0,0);$pdf->Cell(80,12,$vereniging['adres'],0,0);$pdf->Ln(6);
+$pdf->Cell(40,12,'',0,0);$pdf->Cell(80,12,$vereniging['postcode']." ".$vereniging['plaats'],0,0);$pdf->Ln(6);
+$pdf->Cell(40,12,'',0,0);$pdf->Cell(80,12,'KNSA: '.$vereniging['knsa'],0,0);$pdf->Ln(6);
+$pdf->Output("Scoreformulier_".$_GET['knsa'].".pdf", 'I');
 	
-	
-	$pdf->Image('logo_rh.png',10,230,30);
-	$pdf->Cell(40,12,'',0,0);$pdf->Cell(80,12,'SSV Robin Hood',0,0);$pdf->Ln(6);
-	$pdf->Cell(40,12,'',0,0);$pdf->Cell(80,12,'Parklaan 82',0,0);$pdf->Ln(6);
-	$pdf->Cell(40,12,'',0,0);$pdf->Cell(80,12,'3448ZM Woerden',0,0);$pdf->Ln(6);
-	$pdf->Cell(40,12,'',0,0);$pdf->Cell(80,12,'KNSA: 2570',0,0);$pdf->Ln(6);
-	$pdf->Output("Scoreformulier_".$_GET['knsa'].".pdf", 'I');
-	
-	?>
+?>
