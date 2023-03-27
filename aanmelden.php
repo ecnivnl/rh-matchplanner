d6e2671ba559a63dc63afd0bcba9ccfc36b60510
root <root@global03.ecnivconcepts.nl>
2023-03-27 13:14:52 +0200

    New registration form

---
+++ aanmelden.php
@@ -0,0 +1,18 @@
+</php
+
+
+
+
+
+
+?>
+
+
+<html>
+<head>
+</head>
+<body>
+<form action="aanmelden.php">
+  KNSA-licentienummer: <input type="text" name="knsa" value=""><br>
+  <input type="submit" value="Submit">
+</form>
